6ed71d46d5969027ed62233f2ebd12ca6422f9ca
MAIARA TORCHELSEN SARAIVA <01609582020@senacrs.edu.br>
2025-08-21 11:31:47 -0300

    Adiciona construtor

--- Clientes.ts
+++ Clientes.ts
@@ -1,5 +1,10 @@
 export class Cliente {
-  nome: string = "";
-  telefone: string = "";
+  nome: string;
+  telefone: string;
 
+//metodos especial que executa apenas uma vez , que é ao criar o objeto
+  constructor(nomeCliente: string = "", telefone: string = "") {
+    this.nome = nomeCliente;
+    this.telefone = "";
+}
 }
--- Main.ts
+++ Main.ts
@@ -1,3 +1,9 @@
 import { Cliente } from "./Clientes";
 
-const cli: Cliente = new Cliente();
+const cli: Cliente = new Cliente("João");
+      cli.telefone = "987654321";
+      
+      const cli2: Cliente = new Cliente("Maria");
+      cli2.telefone = "123456789";
+
+      console.table(cli);
